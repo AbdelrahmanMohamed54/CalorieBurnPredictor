bc58ee8c7c3d59f46314c36d9ef685b2aed9d9cf
Abdelrahman Mohamed <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2024-02-22 14:47:30 +0400

    Initialize the class CaloriesPredictor as the main GUI window

--- GUI.py
+++ GUI.py
@@ -8,3 +8,12 @@ matplotlib.use('QtAgg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from main import CaloriePredictorModel
+
+
+class CaloriesPredictor(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.defineStyleSheets()
+        self.model = CaloriePredictorModel()
+        self.durations, self.Y_test, self.results_df_tuned = self.model.get_plotting_data()
